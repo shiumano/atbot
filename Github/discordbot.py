b53d3a4c6826d1946305f435d06443562b94e5ae
takkunat <takkunmail2005@gmail.com>
2020-11-28 15:26:41 +0900

    kondokoso

---
+++ Github/discordbot.py
@@ -0,0 +1,22 @@
+import discord, importlib
+import botsystem
+import os
+token = os.environ['DISCORD_BOT_TOKEN']
+
+client = discord.Client()
+botsystem.set_client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user}としてログインしました')
+
+@client.event
+async def on_message(message):
+    if message.content == '@reload':
+        try:
+            importlib.reload(botsystem)
+            await message.channel.send('Reloaded.')
+        except Exception as e:
+            await message.channel.send(f'Not reloaded : {e}')
+    await botsystem.commands(message)
+client.run(token)
