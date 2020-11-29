3744c7b4ddd14d8df892615cb0e931f2ef17261e
takkunat <takkunmail2005@gmail.com>
2020-11-29 15:34:00 +0900

    プレフィックス入れてなかった()

--- discordbot.py
+++ discordbot.py
@@ -24,6 +24,6 @@ async def on_message(message):
             await message.channel.send('Reloaded.')
         except Exception as e:
             await message.channel.send(f'Not reloaded : {e}')
-    await botsystem.commands(message)
+    await botsystem.commands(message,pf)
     await botsystem.zatzudan(message)
 client.run(token)
