fcaccf7088696a3a17c858adf61b8695d30adb3b
takkunat <takkunmail2005@gmail.com>
2021-01-18 00:35:22 +0900

    ツイートしたのにはずかしい

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,7 @@ client=discord.Client()
 async def on_message(m):
  if m.channel.id==726265421803683910:
   while 1:
-   m.channel.send(random.choice(('スマホのスペックってそんなに低いっけ？','スマホを見下すなクソ野郎共','パソコン買う金あればパソコン使うよ')))
+   await m.channel.send(random.choice(('スマホのスペックってそんなに低いっけ？','スマホを見下すなクソ野郎共','パソコン買う金あればパソコン使うよ')))
    if random.randint(1,10000)==1:
-    m.channel.send('@everyone')
+    await m.channel.send('@everyone')
 client.run(token)
