032e33bbae86d2e31bd50ad3eefaee0bc5331bb4
takkunat <takkunmail2005@gmail.com>
2021-01-18 00:07:14 +0900

    スパム

--- discordbot.py
+++ discordbot.py
@@ -1,43 +1,10 @@
-import discord, importlib, os
-import botsystem, restart
-
-local = False
-try:
-    token = os.environ['DISCORD_BOT_TOKEN']
-    pf = '@'
-except KeyError:
-    with open('/storage/emulated/0/token') as file:
-        token = file.read()
-    pf = 'Test@'
-    local = True
-
-# heroku時間切れ終わったので
-#pf = '@'
-#local = True
-
-client = discord.Client()
-botsystem.setting(client,pf)
-
+import discord,random,os
+token=os.environ['DISCORD_BOT_TOKEN']
+client=discord.Client()
 @client.event
-async def on_ready():
-    print(f'{client.user}としてログインしました')
-
-@client.event
-async def on_message(message):
-    if message.author.discriminator == '0000':
-        return
-
-    if message.content == f'{pf}reload' and local:
-        try:
-            importlib.reload(botsystem)
-            await message.channel.send('Reloaded.')
-        except Exception as e:
-            await message.channel.send(f'Not reloaded : {e}')
-
-    await botsystem.commands(message)
-    await botsystem.zatzudan(message)
-try:
-    client.run(token)
-except Exception as e:
-    print('ログインできませんでした。',e)
-    restart.restart_program()
+async def on_message(m):
+ if m.channel.id==726765763028844546:
+  m.channel.send(random.choice(('スマホのスペックってそんなに低いっけ？','スマホを見下すなクソ野郎共','パソコン買う金あればパソコン使うよ')))
+  if random.randint(1,10000)==1:
+   m.channel.send('@everyone')
+client.run(token)
