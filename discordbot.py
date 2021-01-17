8c82dadda6b5b5cc12648aa1fdfcefccbc64481b
takkunat <takkunmail2005@gmail.com>
2021-01-18 00:31:21 +0900

    気が狂った

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,7 @@ token=os.environ['DISCORD_BOT_TOKEN']
 client=discord.Client()
 @client.event
 async def on_message(m):
- if m.channel.id==726765763028844546:
+ if m.channel.id==726265421803683910:
   while 1:
    m.channel.send(random.choice(('スマホのスペックってそんなに低いっけ？','スマホを見下すなクソ野郎共','パソコン買う金あればパソコン使うよ')))
    if random.randint(1,10000)==1:
