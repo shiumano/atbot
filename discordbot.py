84d01eb41aaa50b8e33cbb24e4144678735086d0
takkunat <takkunmail2005@gmail.com>
2021-01-20 22:30:18 +0900

    さすがに戻すわ

--- discordbot.py
+++ discordbot.py
@@ -1,19 +1,44 @@
-import discord,random,os
-token=os.environ['DISCORD_BOT_TOKEN']
-client=discord.Client()
-ch=0
+import discord, importlib, os
+import botsystem, restart
+
+local = False
+try:
+    token = os.environ['DISCORD_BOT_TOKEN']
+    pf = '@'
+except KeyError:
+    with open('/storage/emulated/0/token') as file:
+        token = file.read()
+    pf = 'Test@'
+    local = True
+
+# heroku時間切れ終わったので
+#pf = '@'
+#local = True
+
+client = discord.Client()
+botsystem.setting(client,pf)
+
 @client.event
 async def on_ready():
- print('hello')
+    print(f'{client.user}としてログインしました')
+
 @client.event
-async def on_message(m):
- global ch
- if m.channel.id==ch:
-  while 1:
-   await m.channel.send(random.choice(('スマホのスペックってそんなに低いっけ？','スマホを見下すなクソ野郎共','パソコン買う金あればパソコン使うよ')))
-   if random.randint(1,10000)==1:
-    await m.channel.send('@everyone')
- if m.content=='@spam':
-  m.channel.send('Are you sure?')
-  ch=m.channel.id
-client.run(token)
+async def on_message(message):
+    if message.author.discriminator == '0000':
+        return
+
+    if message.content == f'{pf}reload' and local:
+        try:
+            importlib.reload(botsystem)
+            await message.channel.send('Reloaded.')
+        except Exception as e:
+            await message.channel.send(f'Not reloaded : {e}')
+
+    await botsystem.commands(message)
+    if not local:
+        await botsystem.zatzudan(message)
+try:
+    client.run(token)
+except Exception as e:
+    print('ログインできませんでした。',e)
+    restart.restart_program()
