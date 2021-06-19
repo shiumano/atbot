5c06f8bafa036058704e0c5c49093e0ff580a5e6
takkunat <takkunmail2005@gmail.com>
2021-06-20 01:35:42 +0900

    アップデートをダウンロードできんかった

--- discordbot.py
+++ discordbot.py
@@ -1,4 +1,4 @@
-import discord, asyncio, aiohttp, time, importlib, os, traceback, sys
+import discord, asyncio, aiohttp, async_timeout, time, importlib, os, traceback, sys
 import botsystem, restart
 
 local = False
